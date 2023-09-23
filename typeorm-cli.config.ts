5d412353a3d440f52a971e02ddca58b58ba8b061
khadgachy07 <khadgachy07@gmail.com>
2023-09-23 21:59:57 +0545

    Config

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -8,7 +8,7 @@ export default new DataSource({
   port: 5432,
   username: process.env.DB_USERNAME || 'tb_wallet',
   password: process.env.DB_PASSWORD || 'MoQNQqeUx1ZFXx9ezmGFjD56Q2rN6t6n',
-  database: process.env.DB_NAME || 'tb-wallet',
+  database: process.env.DB_NAME || 'tb_wallet',
   entities: [join(__dirname, './src/modules/**/entities/', '*.entity.{ts,js}')],
   migrations: [join(__dirname, './src/migrations', '*.{ts,js}')],
   migrationsRun: true,
