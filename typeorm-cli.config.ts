9816849f9e77d42d71ad226aaa9d950b9b2ab541
khadgachy07 <khadgachy07@gmail.com>
2023-09-24 00:42:32 +0545

    Render fix #4

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'dpg-ck7enivsasqs73almqj0-a',
   port: 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'tb-wallet',
+  username: process.env.DB_USERNAME || 'tb_wallet',
+  password: process.env.DB_PASSWORD || 'MoQNQqeUx1ZFXx9ezmGFjD56Q2rN6t6n',
+  database: process.env.DB_NAME || 'tb_wallet',
   entities: [join(__dirname, './src/modules/**/entities/', '*.entity.{ts,js}')],
   migrations: [join(__dirname, './src/migrations', '*.{ts,js}')],
   migrationsRun: true,
