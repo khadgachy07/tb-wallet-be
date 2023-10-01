4ba5c6e451c7c38fa2ef573555223d9f2701f98e
khadgachy07 <khadgachy07@gmail.com>
2023-10-02 01:27:01 +0545

    Database connection

--- database.config.ts
+++ database.config.ts
@@ -3,11 +3,11 @@ import { join } from 'path';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'dpg-ck7enivsasqs73almqj0-a',
+  host: process.env.DB_HOST || 'dpg-ckcsm0siibqc73e90m00-a',
   port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'tb_wallet',
-  password: process.env.DB_PASSWORD || 'MoQNQqeUx1ZFXx9ezmGFjD56Q2rN6t6n',
-  database: process.env.DB_NAME || 'tb_wallet',
+  password: process.env.DB_PASSWORD || '83rFOcL3EsNAoX6B2ZlxBO9G02MGpUXd',
+  database: process.env.DB_NAME || 'tb_wallet_q7hj',
   autoLoadEntities: true,
   entities: [join(__dirname, '/src/modules/**/*.entity{.ts,.js}')],
   migrations: [join(__dirname + '../src/**/*.migration{.ts,.js}')],
