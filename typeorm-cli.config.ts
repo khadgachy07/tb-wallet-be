78d71b5eace07f984e16f3dad5d3b9905b853b13
khadgachy07 <khadgachy07@gmail.com>
2023-11-02 18:20:47 +0545

    Database Configure for Hereku

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'dpg-ckcsm0siibqc73e90m00-a',
+  host: process.env.DB_HOST || 'ec2-3-83-61-239.compute-1.amazonaws.com',
   port: 5432,
-  username: process.env.DB_USERNAME || 'tb_wallet',
-  password: process.env.DB_PASSWORD || '83rFOcL3EsNAoX6B2ZlxBO9G02MGpUXd',
-  database: process.env.DB_NAME || 'tb_wallet_q7hj',
+  username: process.env.DB_USERNAME || 'wdgvrqjhrajfoy',
+  password: process.env.DB_PASSWORD || '30a69fbd1e5ca4fea4dfde264705b27191dcbffaf11377986bfca4d99af220cd',
+  database: process.env.DB_NAME || 'dbv339hvg91nsh',
   entities: [join(__dirname, './src/modules/**/entities/', '*.entity.{ts,js}')],
   migrations: [join(__dirname, './src/migrations', '*.{ts,js}')],
   migrationsRun: true,
