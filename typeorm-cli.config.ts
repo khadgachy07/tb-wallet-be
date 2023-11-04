71386d0572a3c728d502657746cbfc24ef2ce081
khadgachy07 <khadgachy07@gmail.com>
2023-11-04 22:30:53 +0545

    Database Configuration changed

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'ec2-3-83-61-239.compute-1.amazonaws.com',
+  host: process.env.DB_HOST || 'postgresql',
   port: 5432,
-  username: process.env.DB_USERNAME || 'wdgvrqjhrajfoy',
-  password: process.env.DB_PASSWORD || '30a69fbd1e5ca4fea4dfde264705b27191dcbffaf11377986bfca4d99af220cd',
-  database: process.env.DB_NAME || 'dbv339hvg91nsh',
+  username: process.env.DB_USERNAME || 'admin',
+  password: process.env.DB_PASSWORD || 'xMUhu6kqsplR7vwKRvvkCgUTjRciXPFh',
+  database: process.env.DB_NAME || 'postgres',
   entities: [join(__dirname, './src/modules/**/entities/', '*.entity.{ts,js}')],
   migrations: [join(__dirname, './src/migrations', '*.{ts,js}')],
   migrationsRun: true,
