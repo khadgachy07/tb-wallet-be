85e96e34c1f152f28287f2d1b6193409bad645da
khadgachy07 <khadgachy07@gmail.com>
2023-11-04 23:55:12 +0545

    Database Config Updated

--- database.config.ts
+++ database.config.ts
@@ -3,11 +3,7 @@ import { join } from 'path';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'postgresql',
-  port: 5432,
-  username: process.env.DB_USERNAME || 'admin',
-  password: process.env.DB_PASSWORD || 'xMUhu6kqsplR7vwKRvvkCgUTjRciXPFh',
-  database: process.env.DB_NAME || 'postgres',
+  url: 'postgres://admin:<xMUhu6kqsplR7vwKRvvkCgUTjRciXPFh>@btph50.stackhero-network.com:5432/admin?sslmode=require',
   autoLoadEntities: true,
   entities: [join(__dirname, '/src/modules/**/*.entity{.ts,.js}')],
   migrations: [join(__dirname + '../src/**/*.migration{.ts,.js}')],
--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -3,11 +3,7 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'postgresql',
-  port: 5432,
-  username: process.env.DB_USERNAME || 'admin',
-  password: process.env.DB_PASSWORD || 'xMUhu6kqsplR7vwKRvvkCgUTjRciXPFh',
-  database: process.env.DB_NAME || 'postgres',
+  url: 'postgres://admin:<xMUhu6kqsplR7vwKRvvkCgUTjRciXPFh>@btph50.stackhero-network.com:5432/admin?sslmode=require',
   entities: [join(__dirname, './src/modules/**/entities/', '*.entity.{ts,js}')],
   migrations: [join(__dirname, './src/migrations', '*.{ts,js}')],
   migrationsRun: true,
